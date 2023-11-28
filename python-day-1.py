64832a11cd04580850d8bb40db414984e10843a5
cloud09 <91081537+sarkeramit2092@users.noreply.github.com>
2023-11-28 07:51:50 +0000

    update day 1

--- python-day-1.py
+++ python-day-1.py
@@ -24,3 +24,24 @@ print (22+7.984)
 print (80/2)
 print (78*22.86)
 print (10%6)
+print(abs(-5))
+print (max(4, 2, 10))
+print (min(2,5,1))
+print (round(4.5))
+print (bin (3))
+
+#math function
+
+from math import *
+
+print (sqrt(1000))
+
+#user input
+
+name1 = input ("Input Your Name: ")
+age = int (input ("Input Your Age: "))
+
+print ("Your name is " + name1 +" and your are", age)
+
+#Word Replacement Exercise
+
